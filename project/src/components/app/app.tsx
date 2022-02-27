23d8f18b6b7e6fc26657fbc3176011c7768dd35f
Anna P <amltrp@mail.ru>
2022-02-27 20:08:29 +0500

    add 3-1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainScreen from '../main-screen/main-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import LoginScreen from '../login-screen/login-screen';
+import PropertyScreen from '../property-screen/property-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   cardsCount: number;
@@ -7,7 +14,34 @@ type AppScreenProps = {
 
 function App({cardsCount, offersCount}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen cardsCount={cardsCount} offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen cardsCount={cardsCount} offersCount={offersCount}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyScreen />}
+        />
+        <Route
+          path='*'
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
